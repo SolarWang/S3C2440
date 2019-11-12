5255915dccba1f3ba19423ad5216f50b9cbeeeec
SolarWang <3540816677@qq.com>
2019-11-12 09:52:16 +0800

    complete nand read function

--- nand flash/nand.c
+++ nand flash/nand.c
@@ -4,34 +4,33 @@
 /*
 *HCLK为100MHZ，每个机器周期为10ns
 */
-#define TACLS  0
+#define TACLS 0
 #define TWRPH0 2
 #define TWRPH1 1
 
-void nand_write_cmd(unsigned char cmd)
+void nand_write_reg_cmd(unsigned char cmd)
 {
     NFCMD = cmd;
 }
 
-void nand_write_addr(unsigned char addr)
+void nand_write_reg_addr(unsigned char addr)
 {
     NFADDR = addr;
 }
 
-unsigned int nand_read_data(void)
+unsigned int nand_read_reg_data(void)
 {
     return NFDATA;
 }
 
-
 void nand_enable_ce(void)
 {
-    NFCONT &= ~(1<<1);
+    NFCONT &= ~(1 << 1);
 }
 
 void nand_disable_ce(void)
 {
-    NFCONT |= (1<<1);
+    NFCONT |= (1 << 1);
 }
 
 void nand_read_id(void)
@@ -43,132 +42,95 @@ void nand_read_id(void)
 
 void nand_wait_ready(void)
 {
-    while(!(NFSTAT&0X01));
+    while (!(NFSTAT & 0X01))
+        ;
 }
 
 void nand_init(void)
 {
-    NFCONF = ((TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4));
-    NFCONT = (1<<4)|(1<<1)|(1<<0);
+    NFCONF = ((TACLS << 12) | (TWRPH0 << 8) | (TWRPH1 << 4));
+    NFCONT = (1 << 4) | (1 << 1) | (1 << 0);
 }
 
 void nand_read_id(void)
 {
-	int i = 0;
-    unsigned char maker_code,device_code;
-	nand_enable_ce();
-	nand_cmd(0x90);
-	nand_addr(0x00);
-	for(i = 0;i < 10;i++);
-	maker_code = nand_read_data();
+    int i = 0;
+    unsigned char maker_code, device_code;
+    nand_enable_ce();
+    nand_cmd(0x90);
+    nand_addr(0x00);
+    for (i = 0; i < 10; i++)
+        ;
+    maker_code = nand_read_data();
     device_code = nand_read_data();
-	nand_disable_ce();
+    nand_disable_ce();
 }
 
-void nand_page(u32 addr)
+void nand_page(u32 page)
 {
     int i = 0;
-    int temp = addr / 2048; 
-    nand_addr(addr);
-    for(i = 0;i < 10;i++);
-    nand_addr(row_addr2);
-    for(i = 0;i < 10;i++);
-    nand_addr(row_addr3);
-    for(i = 0;i < 10;i++);
+
+    nand_write_reg_addr(page & 0xff);
+    for (i = 0; i < 10; i++)
+        ;
+    nand_write_reg_addr((page >> 8) & 0xff);
+    for (i = 0; i < 10; i++)
+        ;
+    nand_write_reg_addr((page >> 16) & 0x1);
+    for (i = 0; i < 10; i++)
+        ;
 }
 
-#define BYTE1(X) *((volatile char *)(&X))
-#define BYTE2(X) *((volatile char *)(&X)+1)
-#define BYTE3(X) *((volatile char *)(&X)+2)
-#define BYTE4(X) *((volatile char *)(&X)+3)
-
-typedef struct 
+void nand_col(u32 col)
 {
-    /* data */
-    unsigned int col1 : 8;
-    unsigned int col2 : 4;
-    unsigned int row1 : 8;
-    unsigned int row2 : 8;
-    unsigned int row3 : 1;
-}nand_addr_field_t;
+    int i = 0;
 
-typedef union
-{
-    int value;
-    nand_addr_field_t field;
-}nand_addr_t;
+    nand_write_reg_addr(col & 0xff);
+    for (i = 0; i < 10; i++)
+        ;
+    nand_write_reg_addr((col >> 8) & 0xf);
+    for (i = 0; i < 10; i++)
+        ;
+}
 
 /*
  *nand flash的一次读操作是由指定地址一直读到page尾，详见ds p31
  *nand falsh的地址并不是完全连续，每行有2112个byte，前2048个byte为数据，
  *2049-2112为ECC内容，2112后有效数据的地址为行+1，列变为0
- *固当bit12为1时，读取的时ECC内容，当bit12为0时，读取的是存储的数据
+ *固当bit11为1时，读取的时ECC内容，当bit11为0时，读取的是存储的数据
  */
-void nand_read(u32 _addr,u32 len,u8 *data)
+void nand_read(u32 _addr, u32 len, u8 *data)
 {
-    nand_addr_t addr;
-    int i=0;
+    int i = 0;
     int col = _addr % 2048;
-    int row = _addr / 2048;
+    /*
+    *这里为什么除以2048，而不是4096呢？
+    *举个例子说明问题。假设我要读取地址为2048的数据，
+    *注意，2048指的是nand flash main区的地址，spare区(OOB)不参与直接的寻址。
+    *它处于nand flash的(page1,col0),如果把这个坐标代入32bit地址中看，地址为
+    *4096,与2048矛盾，在main区寻址时正确的做法是要忽略A11。nand flash寻址要
+    *把A12-A28当成一个数看，A11-A0当成一个数看。
+    */
+    int page = _addr / 2048;
 
-    // addr.value = _addr;
-    
-    // unsigned char col_addr1 = addr.field.col1;
-    // unsigned char col_addr2 = addr.field.col2;
-    // unsigned char row_addr1 = addr.field.row1;
-    // unsigned char row_addr2 = addr.field.row2;
-    // unsigned char row_addr3 = addr.field.row3; 
-
-    while(i < len)
+    while (i < len)
     {
         nand_enable_ce();
-        
-        i++;
+        nand_cmd(0);
+        nand_col(col);
+        nand_page(page);
+        nand_cmd(0x30);
+        nand_wait_ready();
+
+        for (; (i < len) && (col < 2048); col++)
+        {
+            data[i++] = nand_write_reg_data();
+        }
+        if (i >= len)
+            break;
+        col = 0;
+        page++;
         nand_disable_ce();
     }
-
-    
-
 }
 
-
-
-
-
-
-#define PAGE_SIZE 2112
-void nand_read(unsigned int _addr)
-{
-    nand_addr_t addr;
-    unsigned char data[PAGE_SIZE];
-    int i=0;
-
-
-
-    addr.value = _addr;
-    
-
-    unsigned char col_addr1 = addr.field.col1;
-    unsigned char col_addr2 = addr.field.col2;
-    unsigned char row_addr1 = addr.field.row1;
-    unsigned char row_addr2 = addr.field.row2;
-    unsigned char row_addr3 = addr.field.row3;
-    nand_cmd(0);
-    nand_addr(col_addr1);
-    nand_addr(col_addr2);
-    nand_addr(row_addr1);
-    nand_addr(row_addr2);
-    nand_addr(row_addr3);
-    nand_cmd(0x30);
-    nand_wait_ready();
-    for(i = 0;i < PAGE_SIZE;i++)
-    {
-        data[i] = nand_read_data();
-    }
-
-}
-
-void nand_loop(void)
-{
-    
-}
